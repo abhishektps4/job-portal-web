45c0da61be7f56390bb0d7efc0bf821cc0ca9f82
abhishektps4 <abhishek1720044@gmail.com>
2024-12-31 20:48:42 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,7 @@ app.use(cookieParser());
 const corsOptions = {
     //origin:'http://localhost:5173',
     origin: 'https://job-portal-web-frontend.onrender.com',
+    method: ["GET", "POST", "DELETE", "PUT"],
     credentials:true
 } 
 
