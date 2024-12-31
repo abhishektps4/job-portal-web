14e56c68dfccab5b942ce2463743c6105ec82ade
abhishektps4 <abhishek1720044@gmail.com>
2024-12-31 19:52:31 +0530

    Update db.js

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,14 +1,6 @@
 import mongoose from "mongoose";
 
-// const connectDB = async () => {
-//     try {
-//         await mongoose.connect('mongodb+srv://nayan21100:mdJteEvJxMUCqD9O@jobportal.lroullh.mongodb.net/');
-//         console.log('mongodb connected successfully');
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-// export default connectDB;
+
 
 
 
