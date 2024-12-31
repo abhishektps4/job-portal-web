941c0b5e16998e9972076e0c5bccbc38fdd84cbb
abhishektps4 <abhishek1720044@gmail.com>
2024-12-31 15:03:08 +0530

    Update Login.jsx

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -66,7 +66,7 @@ const Login = () => {
                             value={input.email}
                             name="email"
                             onChange={changeEventHandler}
-                            placeholder="abhishek@gmail.com"
+                            placeholder="E-mail"
                         />
                     </div>
                     <div className='my-2'>
@@ -76,7 +76,7 @@ const Login = () => {
                             value={input.password}
                             name="password"
                             onChange={changeEventHandler}
-                            placeholder="abhishek@gmail.com"
+                            placeholder="Password"
                         />
                     </div>
                     <div className='flex items-center justify-between'>
