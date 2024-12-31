7f2d1ff7519124e7af19cc54309866dfb89d15ee
abhishektps4 <abhishek1720044@gmail.com>
2024-12-31 20:57:36 +0530

    Update db.js

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -8,7 +8,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
         try {
-            await mongoose.connect('mongodb+srv://abhishek17200444:aditya182@abhi.zxwqa.mongodb.net');
+            await mongoose.connect('mongodb+srv://nayan21100:mdJteEvJxMUCqD9O@jobportal.lroullh.mongodb.net');
             console.log('mongodb connected successfully');
         } catch (error) {
             console.log(error);
