8ae299d3b3fb1c2d79cdf0df644c27b9f32246ce
abhishektps4 <abhishek1720044@gmail.com>
2025-01-01 14:56:01 +0530

    Update isAuthenticated.js

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -1,25 +1,105 @@
+// import jwt from "jsonwebtoken";
+
+// const isAuthenticated = async (req, res, next) => {
+//     try {
+//         const token = req.cookies.token;
+//         if (!token) {
+//             return res.status(401).json({
+//                 message: "User not authenticated",
+//                 success: false,
+//             })
+//         }
+//         const decode = await jwt.verify(token, "nayan");
+//         if(!decode){
+//             return res.status(401).json({
+//                 message:"Invalid token",
+//                 success:false
+//             })
+//         };
+//         req.id = decode.userId;
+//         next();
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+// export default isAuthenticated;
+
+
+
+
+
+
+
+
+
+
+
+
+
 import jwt from "jsonwebtoken";
 
-const isAuthenticated = async (req, res, next) => {
+// login a user
+export const login = async (req, res) => {
     try {
-        const token = req.cookies.token;
-        if (!token) {
-            return res.status(401).json({
-                message: "User not authenticated",
+        const { email, password, role } = req.body;
+        if (!email || !password || !role) {
+            return res.status(400).json({
+                message: "Something is missing",
+                success: false
+            });
+        };
+
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                message: "Incorrect email or password.",
                 success: false,
             })
         }
-        const decode = await jwt.verify(token, "nayan");
-        if(!decode){
-            return res.status(401).json({
-                message:"Invalid token",
-                success:false
+
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatch) {
+            return res.status(400).json({
+                message: "Incorrect email or password.",
+                success: false,
             })
         };
-        req.id = decode.userId;
-        next();
+
+        // check role is correct or not
+        if (role !== user.role) {
+            return res.status(400).json({
+                message: "Account doesn't exist with current role.",
+                success: false
+            })
+        };
+
+        const tokenData = {
+            userId: user._id // payload with the user id
+        };
+
+        // Generate the JWT token
+        const token = jwt.sign(tokenData, "nayan", { expiresIn: '1d' });  // Token expires in 1 day
+
+        // Send token back to the client (cookie or response body)
+        return res.status(200).cookie("token", token, {
+            maxAge: 1 * 24 * 60 * 60 * 1000,  // Cookie expiry time (1 day)
+            httpOnly: true,  // Ensures the cookie can't be accessed via JavaScript
+            sameSite: 'strict' // Prevents cross-site requests
+        }).json({
+            message: `Welcome back ${user.fullname}`,
+            user: {
+                _id: user._id,
+                fullname: user.fullname,
+                email: user.email,
+                phoneNumber: user.phoneNumber,
+                role: user.role,
+                profile: user.profile
+            },
+            success: true
+        });
+
     } catch (error) {
         console.log(error);
+        return res.status(500).json({ message: "Server error", success: false });
     }
 }
-export default isAuthenticated;
