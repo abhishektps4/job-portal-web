aebf36a4724d909a78216e757ff91792bf020951
abhishektps4 <abhishek1720044@gmail.com>
2025-01-01 16:02:40 +0530

    Update isAuthenticated.js

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -1,106 +1,106 @@
-// import jwt from "jsonwebtoken";
-
-// const isAuthenticated = async (req, res, next) => {
-//     try {
-//         const token = req.cookies.token;
-//         if (!token) {
-//             return res.status(401).json({
-//                 message: "User not authenticated",
-//                 success: false,
-//             })
-//         }
-//         const decode = await jwt.verify(token, "nayan");
-//         if(!decode){
-//             return res.status(401).json({
-//                 message:"Invalid token",
-//                 success:false
-//             })
-//         };
-//         req.id = decode.userId;
-//         next();
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-// export default isAuthenticated;
-
-
-
-
-
-
-
-
-
-
-
-
-
 import jwt from "jsonwebtoken";
 
-// login a user
- const login = async (req, res) => {
+const isAuthenticated = async (req, res, next) => {
     try {
-        const { email, password, role } = req.body;
-        if (!email || !password || !role) {
-            return res.status(400).json({
-                message: "Something is missing",
-                success: false
-            });
-        };
-
-        let user = await User.findOne({ email });
-        if (!user) {
-            return res.status(400).json({
-                message: "Incorrect email or password.",
+        const token = req.cookies.token;
+        if (!token) {
+            return res.status(401).json({
+                message: "User not authenticated",
                 success: false,
             })
         }
-
-        const isPasswordMatch = await bcrypt.compare(password, user.password);
-        if (!isPasswordMatch) {
-            return res.status(400).json({
-                message: "Incorrect email or password.",
-                success: false,
+        const decode = await jwt.verify(token, "nayan");
+        if(!decode){
+            return res.status(401).json({
+                message:"Invalid token",
+                success:false
             })
         };
-
-        // check role is correct or not
-        if (role !== user.role) {
-            return res.status(400).json({
-                message: "Account doesn't exist with current role.",
-                success: false
-            })
-        };
-
-        const tokenData = {
-            userId: user._id // payload with the user id
-        };
-
-        // Generate the JWT token
-        const token = jwt.sign(tokenData, "nayan", { expiresIn: '1d' });  // Token expires in 1 day
-
-        // Send token back to the client (cookie or response body)
-        return res.status(200).cookie("token", token, {
-            maxAge: 1 * 24 * 60 * 60 * 1000,  // Cookie expiry time (1 day)
-            httpOnly: true,  // Ensures the cookie can't be accessed via JavaScript
-            sameSite: 'strict' // Prevents cross-site requests
-        }).json({
-            message: `Welcome back ${user.fullname}`,
-            user: {
-                _id: user._id,
-                fullname: user.fullname,
-                email: user.email,
-                phoneNumber: user.phoneNumber,
-                role: user.role,
-                profile: user.profile
-            },
-            success: true
-        });
-
+        req.id = decode.userId;
+        next();
     } catch (error) {
         console.log(error);
-        return res.status(500).json({ message: "Server error", success: false });
     }
 }
-export default login
+export default isAuthenticated;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import jwt from "jsonwebtoken";
+
+// // login a user
+//  const login = async (req, res) => {
+//     try {
+//         const { email, password, role } = req.body;
+//         if (!email || !password || !role) {
+//             return res.status(400).json({
+//                 message: "Something is missing",
+//                 success: false
+//             });
+//         };
+
+//         let user = await User.findOne({ email });
+//         if (!user) {
+//             return res.status(400).json({
+//                 message: "Incorrect email or password.",
+//                 success: false,
+//             })
+//         }
+
+//         const isPasswordMatch = await bcrypt.compare(password, user.password);
+//         if (!isPasswordMatch) {
+//             return res.status(400).json({
+//                 message: "Incorrect email or password.",
+//                 success: false,
+//             })
+//         };
+
+//         // check role is correct or not
+//         if (role !== user.role) {
+//             return res.status(400).json({
+//                 message: "Account doesn't exist with current role.",
+//                 success: false
+//             })
+//         };
+
+//         const tokenData = {
+//             userId: user._id // payload with the user id
+//         };
+
+//         // Generate the JWT token
+//         const token = jwt.sign(tokenData, "nayan", { expiresIn: '1d' });  // Token expires in 1 day
+
+//         // Send token back to the client (cookie or response body)
+//         return res.status(200).cookie("token", token, {
+//             maxAge: 1 * 24 * 60 * 60 * 1000,  // Cookie expiry time (1 day)
+//             httpOnly: true,  // Ensures the cookie can't be accessed via JavaScript
+//             sameSite: 'strict' // Prevents cross-site requests
+//         }).json({
+//             message: `Welcome back ${user.fullname}`,
+//             user: {
+//                 _id: user._id,
+//                 fullname: user.fullname,
+//                 email: user.email,
+//                 phoneNumber: user.phoneNumber,
+//                 role: user.role,
+//                 profile: user.profile
+//             },
+//             success: true
+//         });
+
+//     } catch (error) {
+//         console.log(error);
+//         return res.status(500).json({ message: "Server error", success: false });
+//     }
+// }
+// export default login
